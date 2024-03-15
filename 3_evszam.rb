12b43af325397179ee4245777d19f89b0f0ac293
Jankai lolo <lolo@Lolo2023>
2024-03-15 22:30:06 +0100

    asd

--- 3_evszam.rb
+++ 3_evszam.rb
@@ -1,7 +1,7 @@
 def szam
-  i = 2024
-  if i % 4 == 0
-   puts 'szökev'
+  i = 2100
+  if (i % 4 == 0 &&  i % 100 != 0 ) ||   i % 400 == 0
+    puts "ez szökő év"
   else
     puts 'ez nem'
   end
--- 4_szam.rb
+++ 4_szam.rb
@@ -1,5 +1,24 @@
 def as
-n = 5431
-n = 
+  num = 64321
+  a = num.digits
+  array =[]
+  array << a.reverse
+  print array
 end
-as()
+# as()
+
+#Másik mód
+
+# def mk
+#   num = 10
+#   b = num.digits.length
+#   i = 0
+#     while i < b do
+#       a = num % 10
+#       i = i + 1
+#       print a
+#     end
+#   array =[]
+#   array << a
+# end
+# mk()
